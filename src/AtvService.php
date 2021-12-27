b2cedc545b8b5cb0b67909d8e6e84dc0b2b4523c
Janne Suominen <janne.suominen@siili.com>
2021-12-27 09:49:02 +0200

    Add data patching + request wrapper for error handling

--- src/AtvService.php
+++ src/AtvService.php
@@ -4,9 +4,11 @@ namespace Drupal\helfi_atv;
 
 use GuzzleHttp\ClientInterface;
 use Drupal\Component\Serialization\Json;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\ServerException;
 
 /**
- * AtvService service.
+ * Communicate with ATV.
  */
 class AtvService {
 
@@ -17,9 +19,16 @@ class AtvService {
    */
   protected ClientInterface $httpClient;
 
+  /**
+   * Headers for requests.
+   *
+   * @var array
+   */
   protected array $headers;
 
   /**
+   * Base endpoint.
+   *
    * @var string
    */
   private string $baseUrl;
@@ -37,20 +46,29 @@ class AtvService {
       'X-Api-Key' => getenv('ATV_API_KEY'),
     ];
 
-    // TODO: figure out tunnistamo based auth to atv
+    // @todo figure out tunnistamo based auth to atv
     $this->baseUrl = getenv('ATV_BASE_URL');
 
   }
 
   /**
+   * Search documents with given arguments.
+   *
    * @param array $searchParams
+   *   Search params.
+   *
+   * @return array
+   *   Data
    */
-  public function searchDocuments(array $searchParams) {
+  public function searchDocuments(array $searchParams): array {
 
-
-    $resp = $this->httpClient->request('GET', $this->buildUrl($searchParams),[
-      'headers' => $this->headers
-    ]);
+    $resp = $this->request(
+      'GET',
+      $this->buildUrl($searchParams),
+      [
+        'headers' => $this->headers,
+      ]
+    );
 
     $responseData = JSON::decode($resp->getBody()->getContents());
 
@@ -58,7 +76,16 @@ class AtvService {
 
   }
 
-  private function buildUrl($params) {
+  /**
+   * Build request url with params.
+   *
+   * @param array $params
+   *   Params for url.
+   *
+   * @return string
+   *   Built url
+   */
+  private function buildUrl(array $params): string {
     $newUrl = $this->baseUrl;
 
     if (!empty($params)) {
@@ -77,39 +104,108 @@ class AtvService {
     return $newUrl;
   }
 
+  /**
+   * List documents for this user.
+   */
   public function listDocuments() {
 
   }
 
-  public function getDocument($id) {
+  /**
+   * Fetch single document with id.
+   *
+   * @param string $id
+   *   Document id.
+   */
+  public function getDocument(string $id) {
 
   }
 
+  /**
+   * Save new document.
+   */
   public function postDocument() {
 
   }
 
-  public function patchDocument($id,$document) {
+  /**
+   * Run PATCH query in ATV.
+   *
+   * @param string $id
+   *   Document id to be patched.
+   * @param array $document
+   *   Document data to update.
+   *
+   * @return bool
+   *   If PATCH succeeded?
+   */
+  public function patchDocument(string $id, array $document): ?bool {
     $patchUrl = $this->baseUrl . $id;
 
-    $resp = $this->httpClient->request(
+    $content = JSON::encode((object) $document);
+
+    return $this->request(
       'PATCH',
       $patchUrl,
       [
         'headers' => $this->headers,
-        'body' => JSON::encode((object) $document),
+        'body' => $content,
       ]
     );
-
   }
 
+  /**
+   * Get document attachements.
+   */
   public function getAttachments() {
 
   }
 
-  public function getAttachment($id) {
+  /**
+   * Get single attachment.
+   *
+   * @param string $id
+   *   Id of attachment.
+   */
+  public function getAttachment(string $id) {
 
   }
 
+  /**
+   * Request wrapper for error handling.
+   *
+   * @param string $method
+   *   Method for request.
+   * @param string $url
+   *   Endpoint.
+   * @param array $options
+   *   Options for request.
+   *
+   * @return bool|array
+   *   Content or boolean if void.
+   */
+  private function request(string $method, string $url, array $options) {
+
+    try {
+      $resp = $this->httpClient->request(
+        $method,
+        $url,
+        $options
+      );
+      if ($resp->getStatusCode() == 200) {
+        if ($method == 'GET') {
+          return $resp->getBody()->getContents();
+        }
+        else {
+          return TRUE;
+        }
+      }
+      return FALSE;
+    }
+    catch (ServerException | GuzzleException $e) {
+      // @todo error handler for ATV request
+      return FALSE;
+    }
+  }
 
 }
