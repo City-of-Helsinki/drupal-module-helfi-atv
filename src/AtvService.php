77cce78d51bfa65c3d0c38df28a106be9a8bb489
Oskari Nevalainen <oskari@mysteerio.fi>
2023-05-16 10:17:31 +0300

    AU-828: Add method to check document existance by transaction id

--- src/AtvService.php
+++ src/AtvService.php
@@ -560,6 +560,38 @@ class AtvService {
   }
 
   /**
+   * Check if documents are found with given Trasaction ID.
+   *
+   * @param string $id
+   *   Transaction id.
+   *
+   * @return bool|null
+   *   Boolean if found.
+   *
+   * @throws \Drupal\helfi_atv\AtvDocumentNotFoundException
+   * @throws \Drupal\helfi_atv\AtvFailedToConnectException
+   * @throws \GuzzleHttp\Exception\GuzzleException|\Drupal\helfi_helsinki_profiili\TokenExpiredException
+   */
+  public function checkDocumentExistsByTransactionId(string $id) {
+    $response = $this->doRequest(
+      'GET',
+      $this->buildUrl('statistics'),
+      [
+        'headers' => $this->headers,
+        'query' => [
+          'transaction_id' => $id
+        ]
+      ]
+    );
+
+    if (isset($response['results'])) {
+      return count($response['results']) === 0;
+    }
+
+    return NULL;
+  }
+
+  /**
    * Parse malformed json.
    *
    * @param string $contentString
