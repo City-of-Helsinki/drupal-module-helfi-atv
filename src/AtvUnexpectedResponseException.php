6192804173003d7c3953e6d339da59168f8b3664
Janne Suominen <janne.suominen@siili.com>
2023-06-02 10:04:22 +0300

    Fix service parameter handling.

--- src/AtvUnexpectedResponseException.php
+++ src/AtvUnexpectedResponseException.php
@@ -3,8 +3,7 @@
 namespace Drupal\helfi_atv;
 
 /**
- * Exception thrown when no ATV response is something
- * we did not except.
+ * Exception thrown when no ATV response is something we did not except.
  */
 class AtvUnexpectedResponseException extends \Exception {
 
