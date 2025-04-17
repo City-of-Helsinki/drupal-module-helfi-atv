d1254e297ed17f65c2d5a17637b21fee1a771e9b
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-04-17 08:42:41 +0300

    UHF-11210: Declare strict types

--- modules/helfi_atv_audit_logging/src/EventSubscriber/AtvServiceEventSubscriber.php
+++ modules/helfi_atv_audit_logging/src/EventSubscriber/AtvServiceEventSubscriber.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\helfi_atv_audit_logging\EventSubscriber;
 
 use Drupal\helfi_atv\Event\AtvServiceExceptionEvent;
--- tests/src/Kernel/AtvServiceTest.php
+++ tests/src/Kernel/AtvServiceTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\Tests\helfi_atv\Kernel;
 
 use Drupal\helfi_atv\AtvAuthFailedException;
